b97a009624e7c0080fc34891ecdd7790f67e9c18
Brandon Long <bl7m6@umsystem.edu>
2020-06-07 00:39:02 -0500

    New setup.py for v3.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
                 'line output, and writing additional commands '
                 'while the process is running.',
     url = 'https://github.com/brandongraylong/PPSP',
-    download_url = 'https://github.com/brandongraylong/PPSP/archive/v3.0.0.tar.gz',
+    download_url = 'https://github.com/brandongraylong/PPSP/archive/v3.0.1.tar.gz',
     author = 'Brandon Long',
     license = 'MIT',
     keywords = ['PERSISTENT', 'SHELL', 'PROCESS'],
